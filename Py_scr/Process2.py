e2ce618ea74ffc1888f41852e66e8c4f7197e9b2
Sanzhar <anapiyaevs0519@gmail.com>
2023-07-01 15:01:14 +0600

    [Py_scr][M] - modified bugs

--- Py_scr/Process2.py
+++ Py_scr/Process2.py
@@ -1,10 +1,10 @@
 import csv
 
-file = open('../Archive/Input2/table2.csv','r')
+file = open('Archive/Input2/table2.csv','r')
 data = list(csv.reader(file, delimiter=","))
 file.close()
 print(data)
 
-with open('../data/out2/output2.csv', 'w', newline='') as csvfile:
+with open('data/out2/output2.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerows(data)
--- Py_scr/Process3.py
+++ Py_scr/Process3.py
@@ -1,10 +1,10 @@
 import csv
 
-file = open('../Archive/Input3/business-operations.csv','r')
+file = open('Archive/Input3/business-operations.csv','r')
 data = list(csv.reader(file, delimiter=","))
 file.close()
 print(data)
 
-with open('../data/out3/output3.csv', 'w', newline='') as csvfile:
+with open('data/out3/output3.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerows(data)
