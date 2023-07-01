f4c9dfda2b730d147aee557d27b1ed240f716c86
Sanzhar <anapiyaevs0519@gmail.com>
2023-07-01 14:06:57 +0600

    [Py_scr][M] - change(...)

--- Py_scr/Process1.py
+++ Py_scr/Process1.py
@@ -1,10 +1,10 @@
 import csv
 
-file = open('../Archive/Input1/overseas-trade.csv','r')
+file = open('.../Archive/Input1/overseas-trade.csv','r')
 data = list(csv.reader(file, delimiter=","))
 file.close()
 print(data)
 
-with open('../data/out1/output1.csv', 'w', newline='') as csvfile:
+with open('.../data/out1/output1.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerows(data)
