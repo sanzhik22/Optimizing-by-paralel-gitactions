87173a58ded8dd8bf46f91a3eba7bc368f1fa2ad
Sanzhar <anapiyaevs0519@gmail.com>
2023-07-01 14:58:56 +0600

    [Process1.py][M] - remove dots

--- Py_scr/Process1.py
+++ Py_scr/Process1.py
@@ -1,10 +1,11 @@
 import csv
+import os
 
-file = open('../Archive/Input1/overseas-trade.csv','r')
+file = open('Archive/Input1/overseas-trade.csv','r')
 data = list(csv.reader(file, delimiter=","))
 file.close()
 print(data)
 
-with open('../data/out1/output1.csv', 'w', newline='') as csvfile:
+with open('data/out1/output1.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerows(data)
