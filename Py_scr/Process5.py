1ec501e18222d605931a2f5c4f62abaeda9cdbb7
Sanzhar <anapiyaevs0519@gmail.com>
2023-07-05 11:49:03 +0600

    fixing

--- Py_scr/Process5.py
+++ Py_scr/Process5.py
@@ -1,6 +1,6 @@
 import csv
 
-file = open('data/out4/out_table_seq1.csv','r')
+file = open('data/out4/out_table_seq2.csv','r')
 data = list(csv.reader(file, delimiter=","))
 file.close()
 print(data)
